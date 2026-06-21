197cfce763c3a67000baae36b841a5207badca5f
Rajat1424 <rajat.aiml2025@glbajajgroup.org>
2026-06-21 20:53:16 +0530

    Day13 assignment is uploaded

---
+++ Day13/Answer_50.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n, i, sum=0;
+    float avg;
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter elements of array:\n");
+    for(i=0; i<n; i++)
+    {
+        scanf("%d", &arr[i]);
+        sum+=arr[i];
+    }
+    avg=(float)sum/n;
+    printf("Sum of array elements: %d\n", sum);
+    printf("Average of array elements: %.2f\n", avg);
+    return 0;
+}
