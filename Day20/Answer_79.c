07fee6d667f64e052b739af451b51f2b47f7b67f
Rajat1424 <rajat.aiml2025@glbajajgroup.org>
2026-06-28 19:35:41 +0530

    Day20 assignment is uploaded

---
+++ Day20/Answer_79.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    a[10][10], r, c, i, j, sum;
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &r, &c);
+    printf("Enter elements of matrix:\n");
+    for(i=0; i<r; i++)
+    {
+        for(j=0; j<c; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+    printf("Row sums:\n");
+    for(i=0; i<r; i++)
+    {
+        sum = 0;
+        for(j=0; j<c; j++)
+        {
+            sum += a[i][j];
+        }
+        printf("Sum of row %d = %d\n", i+1, sum);
+    }
+    return 0;
+}
